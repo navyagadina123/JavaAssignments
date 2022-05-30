c465dfcf21f8da6277ecc33a6e6a66a0a7d9b256
containment zone alerting app <77118145+navyagadina123@users.noreply.github.com>
2022-05-30 21:18:15 +0530

    added String assignment
    StringAssignment

---
+++ src/com/cg/StringBufferAssignment.java
@@ -0,0 +1,23 @@
+package com.cg.javaassignment.strings;
+
+public class StringBufferAssignment {
+
+	public static void main(String[] args) {
+		StringBuffer sb=new StringBuffer();
+		sb.append("StringBuffer ");
+		sb.append("is a peer class of String ");
+		sb.append("that provides much of ");
+		sb.append("the functionality of strings");
+          System.out.println(sb);
+          
+          StringBuffer sb1=new StringBuffer("This method returns the  reversed object on which  it was called");
+          System.out.println("original string: " +sb1);
+          sb1.reverse();
+         System.out.println("reverse string: " +sb1);
+          
+          StringBuffer sb2= new StringBuffer("It is used to _ at the specified position");
+          sb2.insert(14, "insert text");
+          System.out.println(sb2);
+	}
+
+}
